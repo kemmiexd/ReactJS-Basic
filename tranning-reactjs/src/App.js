71e85d7bc91e7867b364a375a22d16a80554d4d0
kemmie <kemmie.xd@gmail.com>
2019-01-10 21:50:43 +0700

    Complete Todolist

--- tranning-reactjs/src/App.js
+++ tranning-reactjs/src/App.js
@@ -1,37 +1,88 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
+import CheckAll from './img/check-all.svg';
 
 class App extends Component {
   constructor () {
     super();
-    let todoItems = [
-      { title: 'Bú Bóng', isChecked: true }, 
-      { title: 'Bán ma túy', isChecked: true }, 
-      { title: 'Tự kỷ' },
-    ];
-
     this.state = {
-      todoItems
-    };
+      todoItems: [
+        { title: 'Bú Bóng', isChecked: true }, 
+        { title: 'Bán ma túy', isChecked: true }, 
+        { title: 'Tự kỷ' },
+      ]
+    }   
 
-    this.onItemClicked = this.onItemClicked.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
-  onItemClicked() {
+  onItemClicked(item) {
+    return (event) => {
+      const isChecked = item.isChecked;
+      const { todoItems } = this.state
+      const index = todoItems.indexOf(item);
+      this.setState({
+        newItem: '',
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isChecked: !isChecked,
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    };
+  }
+
+  onKeyUp (event) {
+    if (event.keyCode === 13) { // enter
+      let text = event.target.value;
+      if (!text) {
+        return;
+      }
+  
+      text = text.trim();
+      if (!text) { return; }
+  
+      this.setState({
+        newItem: '',
+        todoItems: [
+          { title: text, isChecked: false },
+          ...this.state.todoItems
+        ]
+      });
+    }
+  }
+
+  onChange(event) {
     this.setState({
-
+      newItem: event.target.value
     });
-
   }
 
   render() {
-    const { todoItems } = this.state;
+    const { todoItems, newItem } = this.state;
     return (
       <div className="App">
-        { todoItems.length > 0 && todoItems.map((item, index) => (
-            <TodoItem key={index} item={item} onClick={this.onItemClicked} />
-          ))
+        <div className="Header">
+          <img src={CheckAll} width={30} height={30} />
+          <input 
+            type="text" 
+            placeholder="Thứ cần buôn trong ngày" 
+            value={newItem}
+            onChange={this.onChange}
+            onKeyUp={this.onKeyUp} />
+        </div>
+
+        { todoItems.length > 0 && todoItems.map((item, index) => 
+            <TodoItem 
+              key={index} 
+              item={item} 
+              onClick={this.onItemClicked(item)} />
+          )
         }
         { todoItems.length === 0 && 'Chưa tỉnh' }
       </div>
--- tranning-reactjs/src/components/TodoItem.js
+++ tranning-reactjs/src/components/TodoItem.js
@@ -2,16 +2,23 @@
 import React, { Component } from 'react';
 import './TodoItem.css';
 import classNames from 'classnames';
-
+import checkImg from '../img/check.svg';
+import checkedImg from '../img/checked.svg';
 
 class TodoItem extends Component {
   render() {
     const { item, onClick } = this.props;
+    let url = checkImg;
+    if (item.isChecked) {
+      url = checkedImg;
+    }
 
     return (
       <div onClick={onClick} className={classNames('TodoItem', {
         'TodoItem-checked': item.isChecked
       })}>
+
+        <img src={url} />
         <p>{this.props.item.title}</p>
       </div>
     );
